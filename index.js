a353071490201926d5d44090df28ee420b21d2bf
mickaelaustoni <mickael.austoni@gmail.com>
2022-07-29 15:26:18 +0200

    readme

--- index.js
+++ index.js
@@ -1,225 +1,225 @@
 "use strict";
 
 module.exports = {
-    extends: [
-        "airbnb",
-        "eslint:recommended",
-        "plugin:import/errors",
-        "plugin:jsx-a11y/recommended",
-        "plugin:react/recommended",
-        "plugin:react/jsx-runtime",
-        "prettier",
-        "react-app"
+  extends: [
+    "airbnb",
+    "eslint:recommended",
+    "plugin:import/errors",
+    "plugin:jsx-a11y/recommended",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
+    "prettier",
+    "react-app"
+  ],
+  plugins: [
+    "@typescript-eslint",
+    "prettier",
+    "sort-keys-fix"
+  ],
+  rules: {
+    "arrow-body-style": "error",
+    "arrow-parens": "error",
+    "comma-dangle": [
+      "error",
+      "only-multiline"
     ],
-    plugins: [
-        "@typescript-eslint",
-        "prettier",
-        "sort-keys-fix"
+    "complexity": "off",
+    "curly": "off",
+    "dot-notation": "error",
+    "eqeqeq": [
+      "error",
+      "always"
     ],
-    rules: {
-        "arrow-body-style": "error",
-        "arrow-parens": "error",
-        "comma-dangle": [
-            "error",
-            "only-multiline"
-        ],
-        "complexity": "off",
-        "curly": "off",
-        "dot-notation": "error",
-        "eqeqeq": [
-            "error",
-            "always"
-        ],
-        "guard-for-in": "error",
-        "import/extensions": "off",
-        "import/newline-after-import": "error",
-        "import/no-extraneous-dependencies": [
-            "error",
-            {
-                "devDependencies": [
-                    "**/*.test.ts",
-                    "**/*.test.tsx",
-                    "vitest.config.ts"
-                ]
-            }
-        ],
-        "import/no-unresolved": "off",
-        "import/no-useless-path-segments": [
-            "error",
-            {
-                "noUselessIndex": true
-            }
-        ],
-        "import/order": [
-            "error",
-            {
-                "alphabetize": {
-                    "order": "asc",
-                    "caseInsensitive": true
-                },
-                "newlines-between": "never"
-            }
-        ],
-        "indent": [
-            "error",
-            2,
-            {
-                "SwitchCase": 1
-            }
-        ],
-        "jsx-a11y/anchor-is-valid": [
-            "error",
-            {
-                "components": [
-                    "Link"
-                ],
-                "specialLink": [
-                    "hrefLeft",
-                    "hrefRight"
-                ],
-                "aspects": [
-                    "invalidHref",
-                    "preferButton"
-                ]
-            }
-        ],
-        "jsx-quotes": [
-            "error",
-            "prefer-double"
-        ],
-        "max-classes-per-file": [
-            "error",
-            1
-        ],
-        "multiline-comment-style": [
-            "error",
-            "separate-lines"
-        ],
-        "new-parens": "error",
-        "no-bitwise": "error",
-        "no-caller": "error",
-        "no-console": "off",
-        "no-duplicate-imports": "error",
-        "no-eval": "error",
-        "no-multiple-empty-lines": "error",
-        "no-new-wrappers": "error",
-        "no-param-reassign": "error",
-        "no-restricted-imports": [
-            "error",
-            {
-                "patterns": [
-                    {
-                        "group": [
-                            "**/./*",
-                            "!*.module.scss",
-                            "!*.test.tsx"
-                        ],
-                        "message": "No relative import allowed."
-                    }
-                ]
-            }
-        ],
-        "no-restricted-globals": "error",
-        "no-shadow": "error",
-        "no-throw-literal": "error",
-        "no-trailing-spaces": "error",
-        "no-undef-init": "error",
-        "no-unused-expressions": [
-            "error",
-            {
-                "allowShortCircuit": true,
-                "allowTernary": true
-            }
-        ],
-        "no-unused-vars": "off",
-        "no-var": "error",
-        "object-curly-spacing": [
-            "error",
-            "always"
-        ],
-        "object-shorthand": "error",
-        "prefer-const": "error",
-        "prettier/prettier": [
-            "error",
-            {
-                "printWidth": 140
-            }
-        ],
-        "quotes": "off",
-        "quote-props": [
-            "error",
-            "as-needed"
-        ],
-        "radix": "error",
-        "react/function-component-definition": [
-            "error",
-            {
-                "namedComponents": "arrow-function"
-            }
-        ],
-        "react/jsx-filename-extension": [
-            "error",
-            {
-                "extensions": [
-                    ".tsx"
-                ]
-            }
-        ],
-        "react/jsx-pascal-case": "error",
-        "react/react-in-jsx-scope": "off",
-        "react/require-default-props": "off",
-        "sort-keys": "error",
-        "sort-keys-fix/sort-keys-fix": "warn",
-        "spaced-comment": [
-            "error",
-            "always"
+    "guard-for-in": "error",
+    "import/extensions": "off",
+    "import/newline-after-import": "error",
+    "import/no-extraneous-dependencies": [
+      "error",
+      {
+        "devDependencies": [
+          "**/*.test.ts",
+          "**/*.test.tsx"
         ]
-    },
-    "overrides": [
-        {
-            "files": [
-                "*.ts",
-                "*.tsx"
-            ],
-            "rules": {
-                "no-undef": "off"
-            }
+      }
+    ],
+    "import/no-unresolved": "off",
+    "import/no-useless-path-segments": [
+      "error",
+      {
+        "noUselessIndex": true
+      }
+    ],
+    "import/order": [
+      "error",
+      {
+        "alphabetize": {
+          "order": "asc",
+          "caseInsensitive": true
         },
-        {
-            "files": [
-                "*.test.tsx"
+        "newlines-between": "never"
+      }
+    ],
+    "indent": [
+      "error",
+      2,
+      {
+        "SwitchCase": 1
+      }
+    ],
+    "jsx-a11y/anchor-is-valid": [
+      "error",
+      {
+        "components": [
+          "Link"
+        ],
+        "specialLink": [
+          "hrefLeft",
+          "hrefRight"
+        ],
+        "aspects": [
+          "invalidHref",
+          "preferButton"
+        ]
+      }
+    ],
+    "jsx-quotes": [
+      "error",
+      "prefer-double"
+    ],
+    "max-classes-per-file": [
+      "error",
+      1
+    ],
+    "multiline-comment-style": [
+      "error",
+      "separate-lines"
+    ],
+    "new-parens": "error",
+    "no-bitwise": "error",
+    "no-caller": "error",
+    "no-console": "off",
+    "no-duplicate-imports": "error",
+    "no-eval": "error",
+    "no-multiple-empty-lines": "error",
+    "no-new-wrappers": "error",
+    "no-param-reassign": "error",
+    "no-restricted-imports": [
+      "error",
+      {
+        "patterns": [
+          {
+            "group": [
+              "**/./*",
+              "!*.module.scss",
+              "!*.test.tsx"
             ],
-            "rules": {
-                "no-restricted-imports": "off"
-            }
-        },
-        {
-            "files": [
-                "next-env.d.ts",
-                "react-app-env.d.ts",
-                "vite-env.d.ts"
-            ],
-            "rules": {
-                "spaced-comment": "off"
-            }
-        },
-        {
-            "files": [
-                "src/pages/_app.tsx",
-                "stories/*"
-            ],
-            "rules": {
-                "react/jsx-props-no-spreading": "off"
-            }
-        },
-        {
-            "files": [
-                "*.stories.tsx"
-            ],
-            "rules": {
-                "no-restricted-imports": "off",
-                "react/jsx-props-no-spreading": "off"
-            }
-        }
+            "message": "No relative import allowed."
+          }
+        ]
+      }
+    ],
+    "no-restricted-globals": "error",
+    "no-shadow": "error",
+    "no-throw-literal": "error",
+    "no-trailing-spaces": "error",
+    "no-undef-init": "error",
+    "no-unused-expressions": [
+      "error",
+      {
+        "allowShortCircuit": true,
+        "allowTernary": true
+      }
+    ],
+    "no-unused-vars": "off",
+    "no-var": "error",
+    "object-curly-spacing": [
+      "error",
+      "always"
+    ],
+    "object-shorthand": "error",
+    "prefer-const": "error",
+    "prettier/prettier": [
+      "error",
+      {
+        "printWidth": 140
+      }
+    ],
+    "quotes": "off",
+    "quote-props": [
+      "error",
+      "as-needed"
+    ],
+    "radix": "error",
+    "react/function-component-definition": [
+      "error",
+      {
+        "namedComponents": "arrow-function"
+      }
+    ],
+    "react/jsx-filename-extension": [
+      "error",
+      {
+        "extensions": [
+          ".tsx"
+        ]
+      }
+    ],
+    "react/jsx-pascal-case": "error",
+    "react/react-in-jsx-scope": "off",
+    "react/require-default-props": "off",
+    "sort-keys": "error",
+    "sort-keys-fix/sort-keys-fix": "warn",
+    "spaced-comment": [
+      "error",
+      "always"
     ]
+  },
+  "overrides": [
+    {
+      "files": [
+        "*.ts",
+        "*.tsx"
+      ],
+      "rules": {
+        "no-undef": "off"
+      }
+    },
+    {
+      "files": [
+        "*.test.tsx",
+        "*.stories.tsx"
+      ],
+      "rules": {
+        "no-restricted-imports": "off"
+      }
+    },
+    {
+      "files": [
+        "next-env.d.ts",
+        "react-app-env.d.ts",
+        "vite-env.d.ts"
+      ],
+      "rules": {
+        "spaced-comment": "off"
+      }
+    },
+    {
+      "files": [
+        "src/pages/_app.tsx",
+        "src/stories/**",
+        "*.stories.tsx"
+      ],
+      "rules": {
+        "react/jsx-props-no-spreading": "off"
+      }
+    },
+    {
+      "files": [
+        "setupTests.ts"
+      ],
+      "rules": {
+        "import/no-extraneous-dependencies": "off"
+      }
+    }
+  ]
 };
